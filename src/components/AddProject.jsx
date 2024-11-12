cfc9b97cf0f3fc57f7afebd43f2f885c37e9d748
Electrolytful <erhanulusal@hotmail.com>
2024-11-12 23:40:03 +0000

    styled input component

--- src/components/AddProject.jsx
+++ src/components/AddProject.jsx
@@ -3,20 +3,22 @@ import Input from "./Input";
 export default function AddProject() {
   return (
     <div className="w-[35rem] h-min py-8 px-16 m-auto rounded-lg self-center bg-slate-900 text-slate-100">
-      <menu className="flex items-center justify-end gap-4 my-4">
-        <li>
-          <button>Cancel</button>
-        </li>
-        <li>
-          <button>Save</button>
-        </li>
-      </menu>
-
+      <h2 className="text-xl font-bold uppercase">Enter project details</h2>
       <div>
         <Input label="Title" />
         <Input label="Description" textarea />
         <Input label="Due Date" />
       </div>
+      <menu className="flex items-center justify-end gap-4 my-4">
+        <li>
+          <button className="text-red-500 hover:text-red-800">Cancel</button>
+        </li>
+        <li>
+          <button className="px-6 py-2 rounded-md bg-green-700 text-slate-200 hover:text-slate-50 hover:bg-green-500">
+            Save
+          </button>
+        </li>
+      </menu>
     </div>
   );
 }
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,10 +1,25 @@
 import PropTypes, { any } from "prop-types";
 
 export default function Input({ label, textarea, ...props }) {
+  const inputStyles =
+    "w-full p-1 border-b-2 rounded-sm focus:outline-none text-slate-600";
+
   return (
-    <p>
-      <label htmlFor="">{label}</label>
-      {textarea ? <textarea {...props} /> : <input {...props} />}
+    <p className="flex flex-col gap-1 my-4">
+      <label className="text-sm font-bold uppercase text-slate-300">
+        {label}
+      </label>
+      {textarea ? (
+        <textarea
+          className={inputStyles}
+          autoComplete="on"
+          rows={5}
+          spellCheck
+          {...props}
+        />
+      ) : (
+        <input className={inputStyles} {...props} />
+      )}
     </p>
   );
 }
